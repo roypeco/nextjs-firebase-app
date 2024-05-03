2e5d0c673adfc10e4656118ec4905230f79cec5f
roypeco <roy.peco@gmail.com>
2024-05-03 23:44:07 +0900

    11章Done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from 'recoil'
+import "../styles/globals.scss"
+import '../lib/firebase'
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
