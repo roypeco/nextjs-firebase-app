5c3d0b037c0c5981838538f35efdd44a654488ca
roypeco <roy.peco@gmail.com>
2024-05-05 02:23:53 +0900

    12章Done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from 'recoil'
 import "../styles/globals.scss"
 import '../lib/firebase'
-
+import dayjs from 'dayjs'
+import 'dayjs/locale/ja'
 
 
 export default function App({ Component, pageProps }: AppProps) {
+  dayjs.locale('ja')
   return (
     <RecoilRoot>
       <Component {...pageProps} />
