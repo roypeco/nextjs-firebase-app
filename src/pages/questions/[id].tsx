c7f009f3fc0a725794c0132b2d50df55725f7dbb
roypeco <roy.peco@gmail.com>
2024-05-06 15:11:10 +0900

    add react FormEvent

--- src/pages/questions/[id].tsx
+++ src/pages/questions/[id].tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, FormEvent } from 'react'
 import { useRouter } from 'next/router'
 import { Answer } from '../../models/Answer'
 import {
